47ccdc1a10164e575f4125d97c42fda49d62f20e
Iampetrus <pedroalerri@yahoo.es>
2024-05-21 11:47:57 +0200

    Add index.html Add script.js Add styles.css Initialize Git Repository

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const userInput = document.getElementById('text-input');
+const checkPalindromeBtn = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
+
+const checkForPalindrome = input => {
+  const originalInput = input; // Store for later output
+
+  if (input === '') {
+    alert('Please input a value');
+    return;
+  }
+
+  // Remove the previous result
+  resultDiv.replaceChildren();
+
+  const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+  let resultMsg = `<strong>${originalInput}</strong> ${lowerCaseStr === 
+    [...lowerCaseStr].reverse().join('') ? 'is a palindrome.' : 'is not a palindrome. <br> Try again you looser.'}`;
+
+  // answer creation
+  const pTag = document.createElement('p');
+  pTag.className = 'user-input';
+  pTag.innerHTML = resultMsg;
+  resultDiv.appendChild(pTag);
+
+  // Show the result.
+  resultDiv.classList.remove('hidden');
+};
+
+// input confirmation 1
+checkPalindromeBtn.addEventListener('click', () => {
+  checkForPalindrome(userInput.value);
+  userInput.value = '';
+});
+
+//input confirmation 2
+userInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    checkForPalindrome(userInput.value);
+    userInput.value = '';
+  }
+});
+
+
+
+
+
+
